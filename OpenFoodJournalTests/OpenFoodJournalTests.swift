95c64b05f6661848b026fc438ce8f974f8c71651
Kevin Chen <kvn.c8888@gmail.com>
2026-03-19 18:09:44 -0700

    Initial Commit

---
+++ OpenFoodJournalTests/OpenFoodJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenFoodJournalTests.swift
+//  OpenFoodJournalTests
+//
+//  Created by Kevin Chen on 3/19/26.
+//
+
+import Testing
+@testable import OpenFoodJournal
+
+struct OpenFoodJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
